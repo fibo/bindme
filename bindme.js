3199748d15ca05526513c77660e60e34e51c148a
fibo <casati_gianluca@yahoo.it>
2017-09-21 12:08:34 +0200

    handle missing func

--- bindme.js
+++ bindme.js
@@ -1,6 +1,8 @@
 function bindme (self) {
   Array.prototype.slice.call(arguments, 1).forEach(function (func) {
-    self[func] = self[func].bind(self)
+    if (self[func]) self[func] = self[func].bind(self)
+    else console.error('Method ' + func + ' is not defined')
   })
 }
+
 module.exports = bindme
